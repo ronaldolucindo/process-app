289f73ae9556863184fc1576d3d710601667cd64
Ronaldo Lucindo <13293540+ronaldolucindo@users.noreply.github.com>
2019-10-27 15:23:57 -0300

    Add api initial settings

---
+++ src/common/services/process-app.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+
+const processApp = axios.create({
+  baseURL: 'https://5db5c2b7f6869d001474a112.mockapi.io'
+});
+
+function addNewProcess(params) {
+  return processApp.post('/processo', { ...params });
+}
+
+function editProcess(params) {
+  return processApp.post(`/processo/${params.id}`, { ...params });
+}
+
+function deleteProcess(id) {
+  return processApp.deleteProcess(`/processo/${id}`, { id });
+}
+
+function getProcess(id) {
+  return processApp.get(`/processo/${id}`, { id });
+}
+
+function searchProcess(term) {
+    const termEnconded = encodeURI(term);
+  return processApp.get(`/processo?q=${termEnconded}`, { termEnconded });
+}
+
+export default {
+  addNewProcess,
+  editProcess,
+  deleteProcess,
+  getProcess,
+  searchProcess
+};
